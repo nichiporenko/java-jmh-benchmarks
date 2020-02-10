f96bf2ddb26a22f28e65e98da7b9a8b849947343
nichiporenko <excelsiorious@gmail.com>
2020-02-10 14:23:28 +0300

    Updated names of regex benchmarks

--- src/main/java/com/nichiporenko/harness/jmh/benchmarks/regular_expressions/RegexComplexMatchesBenchmark.java
+++ src/main/java/com/nichiporenko/harness/jmh/benchmarks/regular_expressions/RegexComplexMatchesBenchmark.java
@@ -10,7 +10,7 @@ import static com.nichiporenko.harness.jmh.utils.RandomUtils.generateRandomIpArr
 
 /**
  * The benchmark tests the average execution time of 100,000 <b>matches</b> operations for a
- * relatively simple pattern with non-capturing, capturing and named-capturing groups.
+ * relatively complex pattern with non-capturing, capturing and named-capturing groups.
  *
  * @author Dmitry Nichiporenko
  */
@@ -20,43 +20,43 @@ import static com.nichiporenko.harness.jmh.utils.RandomUtils.generateRandomIpArr
 @Warmup(time = 1, iterations = 5)
 @Measurement(time = 1, iterations = 5)
 @Fork(warmups = 1, value = 1)
-public class RegexSimpleBenchmark {
+public class RegexComplexMatchesBenchmark {
     private static final Pattern PATTERN_NON_CAPTURING = Pattern.compile(
-            "(?:\\d{1,3}).(?:\\d{1,3}).(?:\\d{1,3}).(?:\\d{1,3})"
+            "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
     );
     private static final Pattern PATTERN_CAPTURING = Pattern.compile(
-            "(\\d{1,3}).(\\d{1,3}).(\\d{1,3}).(\\d{1,3})"
+            "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
     );
     private static final Pattern PATTERN_NAMED_CAPTURING = Pattern.compile(
-            "(?<name1>\\d{1,3}).(?<name2>\\d{1,3}).(?<name3>\\d{1,3}).(?<name4>\\d{1,3})"
+            "(?<g1>(?<g2>25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?<g3>25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
     );
-    private String[] strings;
+    private String[] ipAddresses;
 
     @Param(value = {"100000"})
     private int PATTERN_CHECKS;
 
     @Setup(Level.Trial)
     public void test() {
-        strings = generateRandomIpArray(PATTERN_CHECKS);
+        ipAddresses = generateRandomIpArray(PATTERN_CHECKS);
     }
 
     @Benchmark
-    public void simpleNonCapturing(final Blackhole blackhole) {
-        for (String ip : strings) {
+    public void complexNonCapturing(final Blackhole blackhole) {
+        for (String ip : ipAddresses) {
             blackhole.consume(PATTERN_NON_CAPTURING.matcher(ip).matches());
         }
     }
 
     @Benchmark
-    public void simpleCapturing(final Blackhole blackhole) {
-        for (String ip : strings) {
+    public void complexCapturing(final Blackhole blackhole) {
+        for (String ip : ipAddresses) {
             blackhole.consume(PATTERN_CAPTURING.matcher(ip).matches());
         }
     }
 
     @Benchmark
-    public void simpleNamedCapturing(final Blackhole blackhole) {
-        for (String ip : strings) {
+    public void complexNamedCapturing(final Blackhole blackhole) {
+        for (String ip : ipAddresses) {
             blackhole.consume(PATTERN_NAMED_CAPTURING.matcher(ip).matches());
         }
     }
--- src/main/java/com/nichiporenko/harness/jmh/benchmarks/regular_expressions/RegexSimpleMatchesBenchmark.java
+++ src/main/java/com/nichiporenko/harness/jmh/benchmarks/regular_expressions/RegexSimpleMatchesBenchmark.java
@@ -10,7 +10,7 @@ import static com.nichiporenko.harness.jmh.utils.RandomUtils.generateRandomIpArr
 
 /**
  * The benchmark tests the average execution time of 100,000 <b>matches</b> operations for a
- * relatively complex pattern with non-capturing, capturing and named-capturing groups.
+ * relatively simple pattern with non-capturing, capturing and named-capturing groups.
  *
  * @author Dmitry Nichiporenko
  */
@@ -20,43 +20,43 @@ import static com.nichiporenko.harness.jmh.utils.RandomUtils.generateRandomIpArr
 @Warmup(time = 1, iterations = 5)
 @Measurement(time = 1, iterations = 5)
 @Fork(warmups = 1, value = 1)
-public class RegexComplexBenchmark {
+public class RegexSimpleMatchesBenchmark {
     private static final Pattern PATTERN_NON_CAPTURING = Pattern.compile(
-            "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+            "(?:\\d{1,3}).(?:\\d{1,3}).(?:\\d{1,3}).(?:\\d{1,3})"
     );
     private static final Pattern PATTERN_CAPTURING = Pattern.compile(
-            "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+            "(\\d{1,3}).(\\d{1,3}).(\\d{1,3}).(\\d{1,3})"
     );
     private static final Pattern PATTERN_NAMED_CAPTURING = Pattern.compile(
-            "(?<g1>(?<g2>25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?<g3>25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+            "(?<name1>\\d{1,3}).(?<name2>\\d{1,3}).(?<name3>\\d{1,3}).(?<name4>\\d{1,3})"
     );
-    private String[] ipAddresses;
+    private String[] strings;
 
     @Param(value = {"100000"})
     private int PATTERN_CHECKS;
 
     @Setup(Level.Trial)
     public void test() {
-        ipAddresses = generateRandomIpArray(PATTERN_CHECKS);
+        strings = generateRandomIpArray(PATTERN_CHECKS);
     }
 
     @Benchmark
-    public void complexNonCapturing(final Blackhole blackhole) {
-        for (String ip : ipAddresses) {
+    public void simpleNonCapturing(final Blackhole blackhole) {
+        for (String ip : strings) {
             blackhole.consume(PATTERN_NON_CAPTURING.matcher(ip).matches());
         }
     }
 
     @Benchmark
-    public void complexCapturing(final Blackhole blackhole) {
-        for (String ip : ipAddresses) {
+    public void simpleCapturing(final Blackhole blackhole) {
+        for (String ip : strings) {
             blackhole.consume(PATTERN_CAPTURING.matcher(ip).matches());
         }
     }
 
     @Benchmark
-    public void complexNamedCapturing(final Blackhole blackhole) {
-        for (String ip : ipAddresses) {
+    public void simpleNamedCapturing(final Blackhole blackhole) {
+        for (String ip : strings) {
             blackhole.consume(PATTERN_NAMED_CAPTURING.matcher(ip).matches());
         }
     }
