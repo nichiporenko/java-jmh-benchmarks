65f379087de6bbaa9abc8c2d64bd8385be084e29
nichiporenko <excelsiorious@gmail.com>
2020-02-26 18:40:29 +0300

    Added stream, string benchmarks

---
+++ src/main/java/com/nichiporenko/harness/jmh/benchmarks/streams/StreamSkipBenchmark.java
@@ -0,0 +1,38 @@
+package com.nichiporenko.harness.jmh.benchmarks.streams;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+/**
+ * The benchmark tests the average execution time of stream's skip method of 99_000_000
+ * elements on sequential and parallel streams with 100_000_000 elements.
+ *
+ * @author Dmitry Nichiporenko
+ */
+@State(value = Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(time = 1, iterations = 5)
+@Measurement(time = 1, iterations = 5)
+@Fork(warmups = 1, value = 1)
+public class StreamSkipBenchmark {
+
+    @Benchmark
+    public void sequentialSkip(Blackhole blackhole) {
+        blackhole.consume(IntStream.range(0, 100_000_000)
+                .skip(99_000_000)
+                .sum());
+    }
+
+    @Benchmark
+    public void parallelSkip(Blackhole blackhole) {
+        blackhole.consume(IntStream.range(0, 100_000_000)
+                .parallel()
+                .skip(99_000_000)
+                .sum());
+    }
+
+}
---
+++ src/main/java/com/nichiporenko/harness/jmh/benchmarks/strings/StringBuilderPrependBenchmark.java
@@ -0,0 +1,41 @@
+package com.nichiporenko.harness.jmh.benchmarks.strings;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The benchmark tests the average execution time of prepending the string
+ * to non-empty StringBuilder instance in different ways.
+ *
+ * @author Dmitry Nichiporenko
+ */
+@State(value = Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(time = 1, iterations = 5)
+@Measurement(time = 1, iterations = 5)
+@Fork(warmups = 1, value = 1)
+public class StringBuilderPrependBenchmark {
+
+    @Param(value = "INSERTED_PART_INSERTED_PART_INSERTED_PART|")
+    private String stringToPrepend;
+    private StringBuilder stringBuilder;
+
+    @Setup
+    public void setup() {
+        stringBuilder = new StringBuilder();
+        stringBuilder.append("EXAMPLE_OF_STRING_EXAMPLE_OF_STRING_EXAMPLE_OF_STRING_EXAMPLE_OF_STRING_EXAMPLE_OF_STRING");
+    }
+
+    @Benchmark
+    public void insert(Blackhole blackhole) {
+        blackhole.consume(stringBuilder.insert(0, stringToPrepend));
+    }
+
+    @Benchmark
+    public void reverse(Blackhole blackhole) {
+        blackhole.consume(stringBuilder.reverse().append(new StringBuilder(stringToPrepend).reverse()).reverse());
+    }
+}
